2237419a76e1d3b77170b7e135ac0efa39b9c618
miklau <miklau@cs.umass.edu>
2018-09-10 11:17:41 -0400

    added comments

--- examples/census.py
+++ examples/census.py
@@ -4,27 +4,33 @@ import numpy as np
 
 # create a Kronecker product workload from dense matrix building blocks
 
+# this is a 2d example:
 domain = (10,25)
 
+# densely represented sub-workloads in each of the dimensions
 identity1 = workload.Matrix( np.eye(10) )
 identity2 = workload.Matrix( np.eye(25) )
 total = workload.Matrix( np.ones((1,10)) )
 prefix = workload.Matrix( np.tril(np.ones((25,25))) )
 
+# form the kron products in each dimension
 W1 = workload.Kron([identity1, identity2])
 W2 = workload.Kron([total, prefix])
+
+# form the union of krons
 W = workload.Concat([W1, W2])
 
 # find a Kronecker product strategy by optimizing the workload
 ps = [2,2] # parameter for P-Identity strategies
 strategy = templates.KronPIdentity(domain, ps)
 
+# run optimization
 strategy.optimize(W)
 
-# get the sparse representation of the optimized strategy
+# get the sparse, explicit representation of the optimized strategy
 A = strategy.sparse_matrix()
 
-# Skip for Lapalce Mechanism, Round for Geometric Mechanism
+# Round for Geometric Mechanism (skip this is using Laplace Mechanism)
 A = np.round(A*1000) / 1000.0
 
 print(A)
