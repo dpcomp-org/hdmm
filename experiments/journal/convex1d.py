b70b337ff6daadb2a5a6ed2d51240afbcccb18dd
Ryan McKenna <rmckenna21@gmail.com>
2019-05-11 08:05:31 -0400

    all norm k

--- experiments/journal/convex1d.py
+++ experiments/journal/convex1d.py
@@ -1,19 +1,25 @@
-from hdmm.workload import Prefix, AllRange, WidthKRange
+from hdmm.workload import Prefix, AllRange, WidthKRange, Permuted, AllNormK, ExplicitGram
 from hdmm.templates import YuanConvex, McKennaConvex
 import numpy as np
 
-base = '/home/ryan/Desktop/strategies'
+base = '/Users/rmckenna/Desktop/strategies'
+
+def norm32(n):
+    WtW = AllNormK(n, 32).gram().dense_matrix()
+    return ExplicitGram(WtW / WtW.min())
 
 workloads = {}
-workloads['all-range'] = AllRange
-workloads['prefix'] = Prefix
-workloads['width32'] = lambda n: WidthKRange(n, 32)
+#workloads['all-range'] = AllRange
+#workloads['prefix'] = Prefix
+#workloads['width32'] = lambda n: WidthKRange(n, 32)
+workloads['permuted'] = lambda n: Permuted(AllRange(n))
+workloads['norm32'] = norm32
 
 for n in [64, 128, 256, 512, 1024, 2048, 4096, 8192]:
     for workload, matrix in workloads.items():
         print(workload, n)
         W = matrix(n)
-        temp = McKennaConvex() #YuanConvex()
+        temp = McKennaConvex(n) #YuanConvex()
         temp.optimize(W) 
         A = temp.strategy().dense_matrix()
         np.save('%s/%s-%d-approx.npy' % (base, workload, n), A)
